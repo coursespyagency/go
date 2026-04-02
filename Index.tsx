160667d596f4cde2f17320c681c37fb406f81848
coursespyagency <mybigmalls1@gmail.com>
2026-04-02 11:17:20 +0530

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,25 @@
+import TopBanner from "@/components/TopBanner";
+import HeroSection from "@/components/HeroSection";
+import StatsSection from "@/components/StatsSection";
+import WhyChooseSection from "@/components/WhyChooseSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import PricingSection from "@/components/PricingSection";
+import FAQSection from "@/components/FAQSection";
+import FinalCTASection from "@/components/FinalCTASection";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <TopBanner />
+      <HeroSection />
+      <StatsSection />
+      <WhyChooseSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <FAQSection />
+      <FinalCTASection />
+    </div>
+  );
+};
+
+export default Index;
